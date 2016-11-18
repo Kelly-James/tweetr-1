e85739a210306a4efb5d6e7547f1b1a864747332
Kelly James <kellyjamesclancy@gmail.com>
2016-11-18 20:34:27 +0000

    Successfully retrieving tweets

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -4,24 +4,27 @@
 const simulateDelay = require("./util/simulate-delay");
 
 // Defines helper functions for saving and` getting tweets, using the database `db`
-module.exports = function makeDataHelpers(client) {
+module.exports = function makeDataHelpers(db) {
   return {
 
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
       simulateDelay(() => {
-        client.db.tweets.insertOne(newTweet);
+        db.tweets.insertOne(newTweet);
         callback(null, true);
       });
     },
 
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
-      // const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-      client.db.collection('tweets').find().toArray(callback);
+      const sortNewestFirst = (a, b) => a.created_at - b.created_at;
+      // console.log('DB in datahelpers: ',
+                  // db.collection('tweets').find().toArray(callback));
+      // db.collection('tweets').find().toArray();
         // console.log(result);
         // return result;
-        // callback(result.sort(sortNewestFirst));
+      db.collection('tweetr').find().toArray(callback);
+        // .sort(sortNewestFirst)
     }
   }
 };
--- server/lib/mongo_db.js
+++ server/lib/mongo_db.js
@@ -4,15 +4,16 @@ const {MongoClient} = require('mongodb');
 const MONGODB_URI = 'mongodb://localhost:27017/tweetr';
 
 const mongoConnect = {
-  connect: function() {
+  connect: function(callback) {
     return MongoClient.connect(MONGODB_URI, (err, db) => {
       if(err) {
         console.error(`Failed to Connect: ${MONGODB_URI}`);
         throw err;
       }
       console.log(`Connected to MongoDB: ${MONGODB_URI}`);
-      // console.log(db)
+      // console.log('db: ', db)
       mongoConnect.db = db;
+      callback(db);
     });
   }
   // db.close();
